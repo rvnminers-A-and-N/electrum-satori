e3b94dc08f4c028e971e6b605f33a3e8ace85f49
rvnminers-A-and-N <86896075+rvnminers-A-and-N@users.noreply.github.com>
2025-09-06 17:30:14 -0400

    EVR -> SAT Update base_crash_reporter.py

--- electrum/base_crash_reporter.py
+++ electrum/base_crash_reporter.py
@@ -57,7 +57,7 @@ class BaseCrashReporter(Logger):
     DESCRIBE_ERROR_MESSAGE = _("Please briefly describe what led to the error (optional):")
     # ASK_CONFIRM_SEND = _("Do you want to send this report?")
     ASK_CONFIM_SEND = _("Sorry, remote sending of crash reports has been temporarily disabled. Please ask for help "
-                         "on the Evrmore discord. (https://discord.gg/4csauGuvw3)")
+                         "on the Satori discord. (https://discord.gg/satoriassociation)")
 
     USER_COMMENT_PLACEHOLDER = _("Do not enter sensitive/private information here. "
                                  "The report will be visible on the public issue tracker.")
