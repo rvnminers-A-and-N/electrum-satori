dbbbd66bbc1940d1ad09ff03492ce861634f464e
rvnminers-A-and-N <86896075+rvnminers-A-and-N@users.noreply.github.com>
2025-09-07 16:34:25 -0400

    EVR -> SAT Update setup.py

--- setup.py
+++ setup.py
@@ -35,9 +35,9 @@ data_files = []
 if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
     # note: we can't use absolute paths here. see #7787
     data_files += [
-        (os.path.join('share', 'applications'),               ['electrum-evrmore.desktop']),
-        (os.path.join('share', 'pixmaps'),                    ['electrum/gui/icons/electrum-evrmore.png']),
-        (os.path.join('share', 'icons/hicolor/128x128/apps'), ['electrum/gui/icons/electrum-evrmore.png']),
+        (os.path.join('share', 'applications'),               ['electrum-satori.desktop']),
+        (os.path.join('share', 'pixmaps'),                    ['electrum/gui/icons/electrum-satori.png']),
+        (os.path.join('share', 'icons/hicolor/128x128/apps'), ['electrum/gui/icons/electrum-satori.png']),
     ]
 
 extras_require = {
@@ -55,7 +55,7 @@ extras_require['fast'] = extras_require['crypto']
 
 
 setup(
-    name="Electrum-Evrmore",
+    name="Electrum-Satori",
     version=version.ELECTRUM_VERSION,
     python_requires='>={}'.format(MIN_PYTHON_VERSION),
     install_requires=requirements,
@@ -70,12 +70,12 @@ setup(
     # package_data kwarg lists what gets put in site-packages when pip installing the tar.gz.
     # By specifying include_package_data=True, MANIFEST.in becomes responsible for both.
     include_package_data=True,
-    scripts=['electrum/electrum-evrmore'],
+    scripts=['electrum/electrum-satori'],
     data_files=data_files,
-    description="Lightweight Evrmore Wallet",
-    author="kralverde",
-    author_email="hans_schm1dt@protonmail.com",
+    description="Lightweight Satori Wallet",
+    author="α | crypticwizardry.com",
+    author_email="austin@echelontg.com, ",
     license="MIT Licence",
-    url="https://github.com/EvrmoreOrg/electrum-evrmore",
-    long_description="""Lightweight Evrmore Wallet""",
+    url="https://github.com/SatoriNetwork/electrum-satori",
+    long_description="""Lightweight Satori Wallet""",
 )
